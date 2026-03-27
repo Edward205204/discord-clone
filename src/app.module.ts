66ca930541492186da07cd5e177a69e2b493527b
Edward205204 <nguyentminhkhoa1@gmail.com>
2026-03-27 21:32:14 +0700

    feat: integrate DatabaseModule into app.module for shared infrastructure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './modules/auth/auth.module'
+import { DatabaseModule } from './shared/infrastructure/database/database.module'
 
+const SharedModules = [DatabaseModule]
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, ...SharedModules],
   controllers: [AppController],
   providers: [AppService],
 })
