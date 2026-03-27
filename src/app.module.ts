fb2535310563534186c0ae1d4c52427ce27b23b5
Edward205204 <nguyentminhkhoa1@gmail.com>
2026-03-27 22:47:27 +0700

    refactor: remove AppController and AppService, updating AppModule to exclude them

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { AuthModule } from './modules/auth/auth.module'
 import { DatabaseModule } from './shared/infrastructure/database/database.module'
 
 const SharedModules = [DatabaseModule]
 @Module({
   imports: [AuthModule, ...SharedModules],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
