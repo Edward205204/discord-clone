b66286e2c6757afbc5bab99b0c5c40d089a1cdf6
Edward205204 <nguyentminhkhoa1@gmail.com>
2026-03-27 21:45:12 +0700

    refactor: simplify environment schema and update database provider to use env config directly

--- src/shared/infrastructure/config/env.schema.ts
+++ src/shared/infrastructure/config/env.schema.ts
@@ -1,17 +1,16 @@
 import z from 'zod'
 
-const envSchema = z
-  .object({
-    POSTGRES_USER: z.string(),
-    POSTGRES_PASSWORD: z.string(),
-    POSTGRES_DB: z.string(),
-    DATABASE_URL: z.string(),
-    MONGO_URI: z.string(),
-    MONGO_INITDB_ROOT_USERNAME: z.string(),
-    MONGO_INITDB_ROOT_PASSWORD: z.string(),
-    REDIS_HOST: z.string(),
-    REDIS_PORT: z.string(),
-    APP_PORT: z.coerce.number(),
-  })
-  .strict()
+const envSchema = z.object({
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string(),
+  MONGO_URI: z.string(),
+  MONGO_INITDB_ROOT_USERNAME: z.string(),
+  MONGO_INITDB_ROOT_PASSWORD: z.string(),
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z.string(),
+  APP_PORT: z.coerce.number(),
+})
+
 export default envSchema
--- src/shared/infrastructure/database/database.provider.ts
+++ src/shared/infrastructure/database/database.provider.ts
@@ -1,16 +1,15 @@
 import { Provider } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { Pool } from 'pg'
 import { DrizzleDb } from './database.types'
 import { DRIZZLE_DB } from './database.constants'
 import * as schema from './schema'
+import { env } from '../config/env.config'
 
 export const DatabaseProvider: Provider = {
   provide: DRIZZLE_DB,
-  inject: [ConfigService],
-  useFactory: (configService: ConfigService): DrizzleDb => {
-    const connectionString = configService.getOrThrow<string>('DATABASE_URL')
+  useFactory: (): DrizzleDb => {
+    const connectionString = env.DATABASE_URL
     const pool = new Pool({
       connectionString,
     })
